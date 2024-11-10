ec53410ba5d4249d9fd56f0fd2409504f55733de
Dominik.Kotecki <dominikus1910@gmail.com>
2024-11-10 14:05:31 +0000

    fix

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/template/html/v2"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+	// Reload the templates on each render, good for development
+	engine.Reload(true) // Optional. Default: false
+
+	// Debug will print each template that is parsed, good for debugging
+	engine.Debug(true) // Optional. Default: false
+	app := fiber.New(fiber.Config{
+		// Pass in Views Template Engine
+		Views: engine,
+		// Enables/Disables access to `ctx.Locals()` entries in rendered views
+		// (defaults to false)
+		PassLocalsToViews: false,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 2!")
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
 	})
 
 	app.Listen(":3000")
