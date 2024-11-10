20156a282c142ed3b23ae313c28ba447c7938434
Dominik.Kotecki <dominikus1910@gmail.com>
2024-11-10 15:04:10 +0000

    add logger

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"github.com/dominikus1993/game-logger/pkg/data"
-	"github.com/gofiber/template/html/v2"
+	"strconv"
 
+	"github.com/dominikus1993/game-logger/pkg/repositories"
+	"github.com/dominikus1993/game-logger/pkg/usecase"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/template/html/v2"
 )
+
+var getGamesUseCase *usecase.GamesUsecase
+
+func init() {
+	getGamesUseCase = usecase.NewGamesUsecase(repositories.NewFakeGamesRepository())
+}
 
 func main() {
 	engine := html.New("./views", ".html")
@@ -29,12 +38,25 @@ func main() {
 	})
 
 	app.Get("/games", func(c *fiber.Ctx) error {
+		page := ParseQueryInt(c.Query("page", "1"), 1)
+		pageSize := ParseQueryInt(c.Query("page_size", "10"), 10)
+		games, err := getGamesUseCase.GetGames(c.Context(), &repositories.GetGamesRequest{Page: page, PageSize: pageSize})
+		if err != nil {
+			log.WithContext(c.Context()).Errorf("Failed to get games: %v", err)
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
 		return c.Render("games", fiber.Map{
-			"Games": []data.Game{
-				{ID: "1", Name: "Game 1", PlayStart: "2021-01-01", PlayEnd: "2021-01-02", Rating: 5, Platform: "PS4"},
-			},
+			"Games": games,
 		})
 	})
 
 	app.Listen(":3000")
+}
+
+func ParseQueryInt(value string, defaultValue int) int {
+	result, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return result
 }
--- pkg/repositories/games.go
+++ pkg/repositories/games.go
@@ -14,3 +14,16 @@ type GetGamesRequest struct {
 type GamesRepository interface {
 	GetGames(ctx context.Context, req *GetGamesRequest) ([]data.Game, error)
 }
+
+type FakeGamesRepository struct {
+}
+
+func NewFakeGamesRepository() *FakeGamesRepository {
+	return &FakeGamesRepository{}
+}
+
+func (r *FakeGamesRepository) GetGames(ctx context.Context, req *GetGamesRequest) ([]data.Game, error) {
+	return []data.Game{
+		{ID: "1", Name: "Game 1", PlayStart: "2021-01-01", PlayEnd: "2021-01-02", Rating: 5, Platform: "PS4"},
+	}, nil
+}
