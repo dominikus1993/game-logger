4aee7452f3084501c90858db9ba877b4040e34e1
Dominik.Kotecki <dominikus1910@gmail.com>
2024-11-15 04:36:23 +0100

    tests

--- tests/GameLogger.Infrastructure.Tests/Repositories/MongoGamesLogsRepositoryTests.cs
+++ tests/GameLogger.Infrastructure.Tests/Repositories/MongoGamesLogsRepositoryTests.cs
@@ -47,6 +47,60 @@ public class MongoGamesLogsRepositoryTests : IClassFixture<MongoDbFixture>, IAsy
         var gameFromDb = games[0];
         Assert.Equivalent(game, gameFromDb);
     }
+    
+    [Fact]
+    public async Task DeleteGameWhenExists()
+    {
+        // Arrange
+
+        var game = new Game()
+        {
+            Id = Guid.NewGuid(), Title = "Cyberpunk 2077", Rating = 8, Platform = "PC",
+            StartDate = DateOnly.FromDateTime(DateTime.Now),
+            FinishDate = DateOnly.FromDateTime(DateTime.Now.AddDays(7)), HoursPlayed = 40
+        };
+        
+        var writeResult = await _repository.WriteGame(game);
+        Assert.True(writeResult.IsSuccess);
+        
+        // Act
+        
+        var deleteResult = await _repository.DeleteGame(game.Id);
+        Assert.True(deleteResult.IsSuccess);
+        
+        var games = await _repository.GetGames(new GetGamesQuery(1, 10));
+        
+        Assert.Empty(games);
+        
+    }
+    
+    [Fact]
+    public async Task DeleteGameWhenNoExists()
+    {
+        // Arrange
+
+        var game = new Game()
+        {
+            Id = Guid.NewGuid(), Title = "Cyberpunk 2077", Rating = 8, Platform = "PC",
+            StartDate = DateOnly.FromDateTime(DateTime.Now),
+            FinishDate = DateOnly.FromDateTime(DateTime.Now.AddDays(7)), HoursPlayed = 40
+        };
+        
+        var writeResult = await _repository.WriteGame(game);
+        Assert.True(writeResult.IsSuccess);
+        
+        // Act
+        
+        var deleteResult = await _repository.DeleteGame(Guid.NewGuid());
+        Assert.False(deleteResult.IsSuccess);
+        Assert.IsType<InvalidOperationException>(deleteResult.ErrorValue);
+        
+        var games = await _repository.GetGames(new GetGamesQuery(1, 10));
+        
+        Assert.NotEmpty(games);
+        Assert.Single(games);
+        
+    }
 
     public Task InitializeAsync()
     {
