b037d762cbb7a07055ee6575aa2af5bc52d2d301
Dominik.Kotecki <dominikus1910@gmail.com>
2024-12-01 16:08:04 +0100

    fix

--- src/GameLogger.AppHost/Program.cs
+++ src/GameLogger.AppHost/Program.cs
@@ -1,3 +1,12 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var database = builder.AddMongoDB("database");
+
+builder.AddProject<GameLogger_Web>("web")
+    .WithHttpEndpoint()
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
