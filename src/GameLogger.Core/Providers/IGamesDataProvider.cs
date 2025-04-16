252cb6fd96dab8dc844c88df99c9cb5dd1f29e72
Dominik.Kotecki <dominikus1910@gmail.com>
2025-04-16 17:52:56 +0200

    remove .net

--- src/GameLogger.Core/Providers/IGamesDataProvider.cs
+++
@@ -1,8 +0,0 @@
-using GameLogger.Core.Data;
-
-namespace GameLogger.Core.Providers;
-
-public interface IGamesDataProvider
-{
-    IAsyncEnumerable<Game> Provide(CancellationToken cancellationToken = default);
-}
