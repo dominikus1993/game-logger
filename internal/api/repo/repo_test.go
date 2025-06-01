2389dad97cc9fac7dd0f608bf9d10b1f7bb69249
Dominik.Kotecki <dominikus1910@gmail.com>
2025-06-01 17:22:20 +0200

    add count method tests

--- internal/api/repo/repo_test.go
+++ internal/api/repo/repo_test.go
@@ -13,6 +13,65 @@ import (
 	"github.com/testcontainers/testcontainers-go/modules/mongodb"
 )
 
+func TestCount(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	// Arrange
+	ctx := context.Background()
+	mongodbContainer, err := mongodb.Run(ctx, "mongo:6")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := mongodbContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	connectionString, err := mongodbContainer.ConnectionString(ctx)
+	if err != nil {
+		t.Fatal(fmt.Errorf("can't download mongo conectionstring, %w", err))
+	}
+	client, err := mongo.NewClient(ctx, connectionString, "Games", "games")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close(ctx)
+	writer := writer.NewMongoGamesWriter(client)
+
+	reader := NewMongoGamesReader(client)
+
+	t.Run("Read when no articles exist", func(t *testing.T) {
+		// Act
+		count, err := reader.Count(ctx)
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, 0, count)
+	})
+
+	t.Run("Read when one article exists", func(t *testing.T) {
+		// Act
+		article := model.Game{
+			Id:          "testArticle",
+			Title:       "testArticle",
+			StartDate:   "2023-10-01",
+			FinishDate:  "2023-11-01",
+			Platform:    "Switch",
+			HoursPlayed: 25,
+			Rating:      5,
+			Notes:       "test notes",
+		}
+		err := writer.WriteGame(ctx, &article)
+		assert.NoError(t, err)
+
+		count, err := reader.Count(ctx)
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, 1, count)
+	})
+}
+
 func TestLoadGame(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
